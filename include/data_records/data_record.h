f28b79cc1a80ef93cade78b992fc90006a7be54d
alex <amd435@drexel.edu>
2016-05-20 12:04:41 -0400

    committing for clion purposes

---
+++ include/data_records/data_record.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+
+#include "sensor_data/sensor_data.h"
+
+const uint_fast8_t ALL = 0xFF;
+
+class Data_Record{
+
+    public:
+        uint_fast8_t = ALL;
+
+        //TODO: make this interface
+        // virtual void print( const ostream & stream ) = 0;
+
+
+
+};
---
+++ include/data_records/record_factory.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "sensor_data/sensor_data.h"
+#include "data_records/data_record.h"
+
+class Record_Factory{
+
+    //TODO: this whole class
+
+};
