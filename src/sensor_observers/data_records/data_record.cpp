be28e6322e7be25cf0ae376e466bed514d742da1
alex <amd435@drexel.edu>
2016-11-03 20:06:12 -0400

    I forgot to put uncomment the line in the data_record.h file, so the log was always blank.

--- src/sensor_observers/data_records/data_record.cpp
+++ src/sensor_observers/data_records/data_record.cpp
@@ -3,7 +3,7 @@
  *  
  *  Creation Date : 10-06-2016
  *
- *  Last Modified : Wed 02 Nov 2016 02:03:19 PM EDT
+ *  Last Modified : Thu 03 Nov 2016 07:53:44 PM EDT
  *
  *  Created By : ronin-zero (浪人ー無)
  *
@@ -47,7 +47,7 @@ string Data_Record::get_separator(){
 
 ostream& operator<<( ostream& s_out, Data_Record const& record ){
 
-        //record.print( s_out );
+        record.print( s_out );
         return s_out;
 }
 
