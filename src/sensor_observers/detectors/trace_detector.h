84ffc05d72c7a7176424e5ec163bf81761acb396
alex <amd435@drexel.edu>
2017-06-19 14:30:22 -0400

    Started the trace detector for real but I have to leave now.

---
+++ src/sensor_observers/detectors/trace_detector.h
@@ -0,0 +1,27 @@
+/*
+ *  File Name : trace_detector.h
+ *  
+ *  Creation Date : 06-16-2017
+ *
+ *  Last Modified : Mon 19 Jun 2017 02:29:47 PM EDT
+ *
+ *  Created By : ronin-zero (浪人ー無)
+ *
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "utils/directory_handler.h"
+#include "sensor_observers/detectors/trace_reader.h"
+#include "sensor_observers/detectors/trace_window.h"
+#include "sensor_observers/detectors/svm_modules/svm_module.h"
+#include "sensor_observers/detectors/linux/syscall_formatters/syscall_formatter.h"
+
+class Trace_Detector {
+
+
+
+}
---
+++ src/trace_detector/trace_detector.h
@@ -0,0 +1,19 @@
+/*
+ *  File Name : trace_detector.h
+ *  
+ *  Creation Date : 06-16-2017
+ *
+ *  Last Modified : Mon 19 Jun 2017 01:32:53 PM EDT
+ *
+ *  Created By : ronin-zero (浪人ー無)
+ *
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "utils/directory_handler.h"
+#include "trace_reader.h"
+
