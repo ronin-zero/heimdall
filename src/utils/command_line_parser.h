8771ea5552bfd8339a5921c6b5001070d3747193
ronin-zero <ronin-zero@github.com>
2019-10-01 02:20:49 -0400

    I worked more on the generalized command line parser but I'm not sure what I'm going to do with it...

--- src/utils/command_line_parser.h
+++ src/utils/command_line_parser.h
@@ -3,7 +3,7 @@
  *  
  *  Creation Date : 09-29-2019
  *
- *  Last Modified : Sun 29 Sep 2019 04:05:22 AM EDT
+ *  Last Modified : Tue 01 Oct 2019 02:20:32 AM EDT
  *
  *  Created By : ronin-zero (浪人ー無)
  *
@@ -13,9 +13,11 @@
 
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <cstdint>
 #include <cstring>
 #include <string>
+#include <iomanip>
 #include <vector>
 
 #include "utils/ascii_operations.h"
@@ -24,19 +26,39 @@ class Command_Line_Parser{
 
     public:
 
-        Command_Line_Parser( int32_t argc, char** argv );
+        virtual Command_Line_Parser(int32_t argc, char** argv) = 0;
         ~Command_Line_Parser() {};
 
-        bool contains_arg( std::string arg );
-        bool contains_option( std::string opt );
-        bool contains_any( std::vector<std::string> args );
+        bool contains_arg(std::string arg);
+        bool contains_option(std::string opt);
+        bool contains_any(std::vector<std::string> args);
 
-        uint_fast32_t num_args() { return arguments.size(); }
+        size_t num_args() { return arguments.size(); }
 
-        std::string arg_at( uint_fast32_t index ) { return arguments.at( index ); }
+        std::string arg_at(size_t index) { return arguments.at(index); }
         std::string get_program_name() { return program_name; }
 
-        int_fast64_t arg_index( std::string arg, uint_fast32_t pos );
+        size_t arg_index(std::string arg, size_t pos=0);
+        size_t option_index(std::string opt, size_t pos=0);
 
+        int_fast32_t get_option_value(std::string opt);
+        std::string get_option_string(std::string opt);
+
+        virtual void print_usage() = 0;
+        virtual bool check_args() = 0;
+
+    protected:
+
+        std::vector<std::string> arguments; // This is just each token in argv as a string in a vector
+        std::string program_name; // This will be the name of the program.  Set as argv[0] in the constructor.
+
+        std::vector<std::string> short_opts;
+        std::vector<std::string> long_opts;
+        std::vector<std::string> short_opt_args;
+        std::vector<std::string> long_opt_args;
+        std::vector<std::string> commands;
+
+
+        
 
 };
